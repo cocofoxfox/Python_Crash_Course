4a51a69829332e3c6d8c05794165ad478cd2779d
ning wei <ning@nings-MacBook-Pro.local>
2020-04-16 20:13:02 -0400

    first commit

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+print("Hello Python World!")
+
+message = "Hello Python world"
+print(message)
+
+#Variable names can contain only letters, numbers and underscores. No spaces
+#They can start with a letter or an underscore, but not with a number.
+#message_1 is right, 1_message is wrong
+#Be careful when using the lowercase l and uppercase letter O, becuase they could be confused with the numbers 1 and 0
+
+name = "ada loveace"
+print(name.title())
+
+first_name = "ada"
+last_name = "lovelace"
+#using f
+full_name = f"{first_name} {last_name}"
+print(full_name)
